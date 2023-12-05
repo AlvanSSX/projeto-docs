18af2e6553fbda7650a6b9b17ed428a5f39f100a
AlvanSSX <137017718+AlvanSSX@users.noreply.github.com>
2023-12-05 11:40:48 -0300

    initial commit

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'projeto ipiranga'
+project = 'docs'
 copyright = '2023, Alvan Xavier'
 author = 'Alvan Xavier'
 
